1df815bf89c79a1cc77f4f3468d92688c7ffaf1b
HoJun Heo <116245224+HeoHoJun@users.noreply.github.com>
2024-09-18 17:48:27 +0900

    js 파일 재수정

--- convert.js
+++ convert.js
@@ -1,7 +1,5 @@
 // 섭씨온도 : celsius   화씨온도 : fahrenheit
 let fah = parseFloat(prompt("변환할 화씨온도는?"));
-let cel;
-
-cel = ((fah - 32) / 1.8).toFixed(1);  // toFixed 함수를 통해 소숫점 자리 수 고정해줌
+let cel = ((fah - 32) / 1.8).toFixed(1);  // toFixed 함수를 통해 소숫점 자리 수 고정해줌
 // cel = ((fah - 32) / 1.8); 소수점 이하 자리가 너무 많이 나옴
 alert(`화씨 ${fah}℉는 섭씨 ${cel}℃입니다.`);
